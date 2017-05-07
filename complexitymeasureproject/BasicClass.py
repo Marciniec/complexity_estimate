e7e7cc48075f444dbf90d477da3b7b1d9ff5c3cf
Anna Marciniec <annamarciniec24601@gmail.com>
2017-05-07 20:05:56 +0200

    trying to make pip package

---
+++ complexitymeasureproject/BasicClass.py
@@ -0,0 +1,16 @@
+from math import ceil, floor
+from random import randint
+
+
+class Basic:
+    def __init__(self):
+        pass
+
+    def set_up(self, l):
+        pass
+
+    def function(self):
+        pass
+
+    def cleaning(self):
+        pass
